2a33d22400c18f7549e6e3924e86db19689ca707
orelsprojects <orelsmail@gmail.com>
2024-04-25 08:34:33 +0300

    Fix auth nav

--- src/app/providers/AuthProvider.tsx
+++ src/app/providers/AuthProvider.tsx
@@ -73,7 +73,7 @@ export default function AuthProvider({
         router.push("/home");
       }
     } else {
-      if (pathname.includes("auth") && pathname.includes("register")) {
+      if (!pathname.includes("auth") && !pathname.includes("register")) {
         router.push("/auth");
       }
     }
