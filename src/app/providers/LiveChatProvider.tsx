0df019b39fa8ebd9885301169a90af6fb531865b
orelsprojects <orelsmail@gmail.com>
2024-05-04 20:47:30 +0300

    live chat will not show on small screen

--- src/app/providers/LiveChatProvider.tsx
+++ src/app/providers/LiveChatProvider.tsx
@@ -1,14 +1,38 @@
-import Script from "next/script";
-import React from "react";
+"use client";
+
+import { useEffect, useState } from 'react';
+import Script from 'next/script';
 
 export default function LiveChatProvider() {
   const key = "2545e27e-7032-47eb-b960-f8a5a50c1663";
+  const [loadScript, setLoadScript] = useState(false);
+
+  useEffect(() => {
+    // Function to update state based on window width
+    const handleResize = () => {
+      if (window.innerWidth >= 640) { // Assuming 640px is your 'sm' breakpoint
+        setLoadScript(true);
+      } else {
+        setLoadScript(false);
+      }
+    };
+
+    // Set initial state and add event listener
+    handleResize();
+    window.addEventListener('resize', handleResize);
+
+    // Cleanup listener on component unmount
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
   return (
-    <Script
-      id="ze-snippet"
-      src={`https://static.zdassets.com/ekr/snippet.js?key=` + key}
-    >
-      {" "}
-    </Script>
+    <>
+      {loadScript && (
+        <Script
+          id="ze-snippet"
+          src={`https://static.zdassets.com/ekr/snippet.js?key=${key}`}
+        />
+      )}
+    </>
   );
 }
