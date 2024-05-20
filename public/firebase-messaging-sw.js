f62f5ed9df7eafb513dfdbee2bf6f827f8683de1
orelsprojects <orelsmail@gmail.com>
2024-05-20 13:30:32 +0300

    fix firebase sw

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,7 +1,5 @@
-importScripts("https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js");
-importScripts(
-  "https://www.gstatic.com/firebasejs/10.12.0/firebase-messaging.js",
-);
+importScripts('https://www.gstatic.com/firebasejs/8.10.1/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/8.10.1/firebase-messaging.js');
 
 const firebaseConfig = {
   apiKey: "AIzaSyALZvbmwKVBUXia4-u2Wv__C6ST6GFbBUQ",
@@ -10,27 +8,29 @@ const firebaseConfig = {
   storageBucket: "myworkout-ca350.appspot.com",
   messagingSenderId: "334976118267",
   appId: "1:334976118267:web:2547d2f91a0235d1aa2f5e",
-  measurementId: "G-BTFG0DLT3J",
+  measurementId: "G-BTFG0DLT3J"
 };
 
-// Firebase Messaging setup and other service worker code here
+// Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
+// Initialize Firebase Messaging
 const messaging = firebase.messaging();
-messaging.onBackgroundMessage(payload => {
-  console.log(
-    "[firebase-messaging-sw.js] Received background message ",
-    payload,
-  );
 
-  const title = payload.data.title;
+// Handle background messages
+messaging.onBackgroundMessage((payload) => {
+  console.log("[firebase-messaging-sw.js] Received background message ", payload);
+
+  // Define the notification options
   const notificationOptions = {
-    body: payload.data.body,
-    icon: payload.data.icon,
+    body: payload.data.body || "No body",
+    icon: payload.data.icon || '/default-icon.png',
     vibrate: [200, 100, 200, 100, 200, 100, 200],
-    tag: "",
-    image: payload.data.image,
-    badge: payload.data.badge,
+    tag: payload.data.tag || '',
+    image: payload.data.image || '',
+    badge: payload.data.badge || ''
   };
-  self.registration.showNotification("LOLOL", notificationOptions);
+
+  // Show notification
+  self.registration.showNotification(payload.data.title || "Notification", notificationOptions);
 });
