066d0de9028dc1f139b607422309bcaf7049eb3e
orelsprojects <orelsmail@gmail.com>
2024-05-29 10:28:52 +0300

    fix layout

--- src/app/(content)/layout.tsx
+++ src/app/(content)/layout.tsx
@@ -6,13 +6,5 @@ interface RootLayoutProps {
 }
 
 export default function Layout({ children }: RootLayoutProps) {
-  useEffect(() => {
-    // use window to set display mode standalone
-    if (window.matchMedia("(display-mode: standalone)").matches) {
-      console.log("display-mode is standalone");
-    }
-    
-  }, []);
-
   return <ContentLayout>{children}</ContentLayout>;
 }
