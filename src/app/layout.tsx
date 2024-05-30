aad173fea63ce86c653a62ced31d876f081bd6a3
orelsprojects <orelsmail@gmail.com>
2024-05-30 16:57:34 +0300

    try to check theme color

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -63,14 +63,12 @@ export default function Layout({ children }: RootLayoutProps) {
         <meta name="mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="default" />
-        <meta name="theme-color" content="#FBF8F400" />
+        <meta name="theme-color" content="#F0FF00" />
         <link rel="manifest" href="/manifest.json" />
         <meta property="og:image" content="<generated>" />
         <meta property="og:image:type" content="<generated>" />
         <meta property="og:image:width" content="<generated>" />
         <meta property="og:image:height" content="<generated>" />
-        <meta name="theme-color" content="#FBF8F400" className="dark:hidden" />
-        <meta name="theme-color" content="#FBF8F400" className="dark" />
       </head>
       <body className="!overscroll-none">
         <LiveChatProvider />
